eed7024d325a23d0e9c5da1ae2b0a296582c6f4b
xiachunjun <xiachunjun@wanda.cn>
2017-12-18 17:04:34 +0800

    no message

---
+++ src/main/java/com/sc/domain/User.java
@@ -0,0 +1,153 @@
+package com.sc.domain;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    /**
+     * 用户代码
+     */
+    private String userCode;
+
+    /**
+     * 登录名
+     */
+    private String userLoginName;
+
+    /**
+     * 密码
+     */
+    private String userLoginPwd;
+
+    /**
+     * 身份证号
+     */
+    private String userIdCard;
+
+    /**
+     * 姓名
+     */
+    private String userName;
+
+    /**
+     * 昵称
+     */
+    private String userNickname;
+
+    private Integer dataState;
+
+    private Integer dataVersion;
+
+    private String createUser;
+
+    private String updateUser;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode == null ? null : userCode.trim();
+    }
+
+    public String getUserLoginName() {
+        return userLoginName;
+    }
+
+    public void setUserLoginName(String userLoginName) {
+        this.userLoginName = userLoginName == null ? null : userLoginName.trim();
+    }
+
+    public String getUserLoginPwd() {
+        return userLoginPwd;
+    }
+
+    public void setUserLoginPwd(String userLoginPwd) {
+        this.userLoginPwd = userLoginPwd == null ? null : userLoginPwd.trim();
+    }
+
+    public String getUserIdCard() {
+        return userIdCard;
+    }
+
+    public void setUserIdCard(String userIdCard) {
+        this.userIdCard = userIdCard == null ? null : userIdCard.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname == null ? null : userNickname.trim();
+    }
+
+    public Integer getDataState() {
+        return dataState;
+    }
+
+    public void setDataState(Integer dataState) {
+        this.dataState = dataState;
+    }
+
+    public Integer getDataVersion() {
+        return dataVersion;
+    }
+
+    public void setDataVersion(Integer dataVersion) {
+        this.dataVersion = dataVersion;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser == null ? null : createUser.trim();
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser == null ? null : updateUser.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
--- src/test/java/MapperGenerator.java
+++ src/test/java/MapperGenerator.java
@@ -8,23 +8,23 @@ import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class MapperGenerator {
-	public static void main(String[] args) {
-		List<String> warnings = new ArrayList<String>();
-		final boolean overwrite = true;
-		File configFile = new File("/Users/xcj/git/ggstudy/ggstudy-web/src/test/resources/mysqlGeneratorConfig.xml");
-		
-		// System.out.println("config fiel is in : " +
-		// configFile.getAbsoluteFile());
-		ConfigurationParser cp = new ConfigurationParser(warnings);
-		Configuration configuration;
-		try {
-			configuration = cp.parseConfiguration(configFile);
-			DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-			MyBatisGenerator mybatisGenerator = new MyBatisGenerator(configuration, callback, warnings);
-			mybatisGenerator.generate(null);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public static void main(String[] args) {
+        List<String> warnings = new ArrayList<String>();
+        final boolean overwrite = true;
+        File configFile = new File("/Users/xcj/git/sc-manage/src/test/resources/mysqlGeneratorConfig.xml");
 
-	}
+        // System.out.println("config fiel is in : " +
+        // configFile.getAbsoluteFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration configuration;
+        try {
+            configuration = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator mybatisGenerator = new MyBatisGenerator(configuration, callback, warnings);
+            mybatisGenerator.generate(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
