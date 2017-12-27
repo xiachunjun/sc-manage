301779a12d8bf2b0fa25ba2644592b6cdd2dcd7f
xiachunjun <xiachunjun@wanda.cn>
2017-12-27 16:09:26 +0800

    no message

--- src/main/java/com/sc/service/IDutyService.java
+++ src/main/java/com/sc/service/IDutyService.java
@@ -1,47 +1,6 @@
 package com.sc.service;
 
-import java.util.Map;
-
-import com.sc.model.request.PositionModel;
-
 public interface IDutyService {
-
-	/**
-	 * 新增责任清单
-	 * @param positionModel
-	 */
-	void savePosition(PositionModel positionModel);
-
-	/**
-	 * 条件查询责任清单
-	 * @param positionModel
-	 * @return
-	 */
-	Map<String, Object> queryDutyByCondition(PositionModel positionModel);
-
-	/**
-	 * 修改责任清单
-	 * @param positionModel
-	 */
-	void updatePosition(PositionModel positionModel);
-
-	/**
-	 * 查询部门列表
-	 * @return
-	 */
-	Map<String, Object> queryDepartmentList();
-
-	/**
-	 * 查询岗位列表
-	 * @return
-	 */
-	Map<String, Object> queryPositionList();
-	
-	/**
-	 * 编辑岗位责任人
-	 * @return
-	 */
-	public void editPosition(PositionModel positionModel);
 
 	
 }
--- src/main/java/com/sc/service/IPositionService.java
+++ src/main/java/com/sc/service/IPositionService.java
@@ -1,8 +1,10 @@
 package com.sc.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.sc.domain.Position;
+import com.sc.model.request.PositionModel;
 
 public interface IPositionService {
 	/**
@@ -18,4 +20,30 @@ public interface IPositionService {
 	 * @return
 	 */
 	List<Position> queryByDeptCode(String deptCode);
+	
+	/**
+	 * 新增责任清单
+	 * @param positionModel
+	 */
+	void savePosition(PositionModel positionModel, String userLoginName);
+
+	/**
+	 * 条件查询责任清单
+	 * @param positionModel
+	 * @return
+	 */
+	Map<String, Object> queryPositionByCondition(PositionModel positionModel);
+
+	/**
+	 * 修改责任清单
+	 * @param positionModel
+	 */
+	void updatePosition(PositionModel positionModel, String userLoginName);
+	
+	/**
+	 * 编辑岗位责任人
+	 * @return
+	 */
+	public void editPosition(PositionModel positionModel);
+	
 }
