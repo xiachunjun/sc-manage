0563f91c2f5f0d6f24e38688e6558990e8582bf7
wangqiulin <wangqiulin92@163.com>
2018-01-06 21:50:07 +0800

    mytree,

--- src/main/resources/static/assets/js/myjs/mytree.js
+++ src/main/resources/static/assets/js/myjs/mytree.js
@@ -35,51 +35,27 @@ var tree = {
 					expand:function(event, treeId, treeNode){
 						//如果点击的节点没子节点
 						if(!tree.operation.tree.zTree.getNodeByParam("pid", treeNode.tid)){
-							
 							tree.operation.tree.ajaxMethod({
 								url: "/tree/show/node/byPid",
-								data: {
-									pid : treeNode.tid
-								},
+								data: {pid : treeNode.tid},
 								callback: function(data){
 									tree.operation.tree.zTree.addNodes(treeNode, data.dataMap.treeList, true)
 								}
 							})
-							
-//							$.post("/tree/show/node/byPid", {pid:treeNode.tid}, function(data){
-//								//追加到父节点
-//								tree.operation.tree.zTree.addNodes(treeNode, data.dataMap.treeList, true);	
-//							})
 						}
 					}
 				}
 			},
 			
-			
-			/**一次性加载*/
-			loadTree:function(){
-				$.post("/tree/query/all", null ,function(data){
-					$("#tree").zTree(tree.operation.tree.setting, data.dataMap.treeList);	
-				})
-			},
-			
-			
 			/**加载根节点*/
 			loadRoot:function(){
 				tree.operation.tree.ajaxMethod({
 					url: "/tree/show/node/byPid",
-					data: {
-						pid : 0
-					},
+					data: {pid : 0},
 					callback: function(data){
 						tree.operation.tree.zTree = $("#tree").zTree(tree.operation.tree.setting, data.dataMap.treeList);	
 					}
 				})
-				
-//				$.post("/tree/show/node/byPid", {pid:0}, function(data){
-//					tree.operation.tree.zTree = $("#tree").zTree(tree.operation.tree.setting, data.dataMap.treeList);	
-//				})
-				
 			},
 			
 			/**简单封装*/
@@ -94,9 +70,6 @@ var tree = {
 }
 
 $(function(){
-	//一次性加载树
-	//tree.operation.tree.loadTree();
-	
 	//点击事件加载树节点
 	tree.operation.tree.loadRoot();
 })
