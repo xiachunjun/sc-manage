1d88c67b17508b01587d2d6d617c608acc26c152
xiachunjun <xiachunjun@wanda.cn>
2018-01-27 12:01:14 +0800

    no message

--- src/main/java/com/sc/model/request/IDModel.java
+++ src/main/java/com/sc/model/request/IDModel.java
@@ -1,10 +1,13 @@
 package com.sc.model.request;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * 所有根据ID明细查询，删除都使用该model
  *
  */
 public class IDModel {
+	@NotNull(message = "id不能为空")
 	private Integer id;
 
 	public Integer getId() {
